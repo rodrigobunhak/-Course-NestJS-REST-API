0b158d73c6823fc79d58109b311d8b6139a2dcbd
Rodrigo Bunhak <bunhak.rodrigo@gruposervices.com.br>
2022-10-10 15:34:32 -0300

    add auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
+
+    // POST /auth/signup
+    @Post('signup')
+    signup() {
+        return this.authService.signup();
+    }
+
+    // POST /auth/signin
+    @Post('signin')
+    signin() {
+        return this.authService.signin();
+    }
 }
