fb5544a099043ef718823e2219b2d5c6e791b055
Rodrigo Bunhak <bunhak.rodrigo@gruposervices.com.br>
2022-10-13 15:25:42 -0300

    add credentials error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
@@ -10,17 +11,28 @@ export class AuthService {
         // generate the password hash
         const hash = await argon.hash(dto.password);
         // save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            })
+            // return the saved user
+            
+            delete user.hash;
+    
+            return user;
+            
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken');
+                }
             }
-        })
-        // return the saved user
-        
-        delete user.hash;
 
-        return user;
+            throw error;
+        }
     }
 
     signin() {
